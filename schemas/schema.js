514401698f25549cb2f12ffa7e0263c697e7875b
unknown <nagaraju.d@techouts.com>
2021-12-20 19:05:17 +0530

    first commit

---
+++ schemas/schema.js
@@ -0,0 +1,124 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: 'default',
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+
+    {
+      name:'author',
+      type:'document',
+      title:'Author',
+      fields:[
+        {
+          name:'name',
+          type:'string',
+          title:'Name'
+        },
+        {
+          name:'avatar',
+          type:'image',
+          title:'Avatar'
+        }
+      ]
+    },
+
+    {
+      name:'blog',
+      title:'My Blog',
+      type:'document',
+      fields:[
+        {
+          name:'title',
+          type:'string',
+          title:'Title',
+        },
+        {
+          name:'subtitle',
+          type:'string',
+          title:'Subtitle'
+        },
+        {
+          name:'coverImage',
+          type:'image',
+          title:'Cover Image',
+          options:{
+            hotspot:true
+          }
+        },
+        {
+          name:'content',
+          title:'Content',
+          type:'array',
+          of:[
+            {
+              type:'block'
+            },
+            {
+              type:'image',
+              fields:[
+                {
+                  title:'Position',
+                  name:'position',
+                  type:'string',
+                  options:{
+                    list:[
+                      {title:'Center', value:'center'},
+                      {title:'Right', value:'right'},
+                      {title:'Left', value:'left'}
+                    ],
+                    layout:'radio',
+                    isHighlighted:true
+                  }
+                },
+                {
+                  title:'Description',
+                  name:'alt',
+                  type:'text',
+                  options:{
+                    isHighlighted:true
+                  }
+                }
+              ],
+              options:{
+                hotspot:true
+              }
+            },
+            {
+              type:'code',
+              options:{
+                withFilename:true
+              }
+            }
+          ]
+        },
+        {
+          name:'date',
+          type:'datetime',
+          title:'Date',
+          validation: Rule => Rule.required()
+        },
+        {
+          name:'author',
+          type:'reference',
+          title:'Author',
+          to:[{type:'author'}],
+          validation: Rule => Rule.required()
+        },
+        {
+          name:'slug',
+          type:'slug',
+          title:'Slug',
+          validation: Rule => Rule.required()
+        }
+      ]
+    }
+  ]),
+})
